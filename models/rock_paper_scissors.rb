dd378951e10ecbff33594dd302e8b136eceed54b
EDownie <53910027+ED1978@users.noreply.github.com>
2019-09-16 19:50:56 +0100

    returns winner

--- models/rock_paper_scissors.rb
+++ models/rock_paper_scissors.rb
@@ -1,13 +1,70 @@
 class RockPaperScissors
 
-  def self.play(hand1, hand2)
-    if hand1 == 'rock' && hand2 == 'paper' or hand1 == 'paper' && hand2 == 'rock'
-      return 'Paper'
-    elsif hand1 == 'rock' && hand2 == 'scissors' or hand1 == 'scissors' && hand2 == 'rock'
-      return "Rock"
-    elsif hand1 == 'scissors' && hand2 == 'paper' or hand1 == 'paper' && hand2 == 'scissors'
-      return 'Scissors'
+  def self.rock_paper_player_1(hand1, hand2)
+    if hand1 == 'paper' && hand2 == 'rock'
+      return true
     end
   end
+
+  def self.rock_paper_player_2(hand1, hand2)
+    if hand1 == 'rock' && hand2 == 'paper'
+      return true
+    end
+  end
+
+  def self.rock_scissors_player_1(hand1, hand2)
+    if hand1 == 'rock' && hand2 == 'scissors'
+      return true
+    end
+  end
+
+  def self.rock_scissors_player_2(hand1, hand2)
+    if hand1 == 'scissors' && hand2 == 'rock'
+      return true
+    end
+  end
+
+  def self.paper_scissors_player_1(hand1, hand2)
+    if hand1 == 'scissors' && hand2 == 'paper'
+      return true
+    end
+  end
+
+  def self.paper_scissors_player_2(hand1, hand2)
+    if hand1 == 'paper' && hand2 == 'scissors'
+      return true
+    end
+  end
+
+  def self.play(hand1, hand2)
+    if
+      rock_paper_player_1(hand1, hand2)
+      player = "Player 1"
+      weapon = "paper"
+    elsif
+      rock_paper_player_2(hand1, hand2)
+      player = "Player 2"
+      weapon = "paper"
+    elsif
+      rock_scissors_player_1(hand1,hand2)
+      player = "Player 1"
+      weapon = "rock"
+    elsif
+      rock_scissors_player_2(hand1,hand2)
+      player = "Player 2"
+      weapon = "rock"
+    elsif
+      paper_scissors_player_1(hand1,hand2)
+      player = "Player 1"
+      weapon = "scissors"
+    elsif
+      paper_scissors_player_2(hand1,hand2)
+      player = "Player 2"
+      weapon = "scissors"
+    end
+    return "#{player} wins with #{weapon}!"
+  end
+
+
 
 end
--- specs/rock_paper_scissors_spec.rb
+++ specs/rock_paper_scissors_spec.rb
@@ -5,17 +5,35 @@ require_relative('../models/rock_paper_scissors.rb')
 class RockPaperScissorsTest < MiniTest::Test
 
   def test_play_rock_paper()
-    assert_equal("Paper",RockPaperScissors.play('rock', 'paper'))
-    assert_equal("Paper",RockPaperScissors.play('paper', 'rock'))
+    assert_equal("Player 1 wins with paper!",RockPaperScissors.play('paper', 'rock'))
+    assert_equal("Player 2 wins with paper!",RockPaperScissors.play('rock', 'paper'))
+    assert_equal("Player 1 wins with rock!",RockPaperScissors.play('rock', 'scissors'))
+    assert_equal("Player 2 wins with rock!",RockPaperScissors.play('scissors', 'rock'))
+    assert_equal("Player 1 wins with scissors!",RockPaperScissors.play('scissors', 'paper'))
+    assert_equal("Player 2 wins with scissors!",RockPaperScissors.play('paper', 'scissors'))
   end
 
-  def test_play_rock_scissors()
-    assert_equal("Rock",RockPaperScissors.play('rock', 'scissors'))
-    assert_equal("Rock",RockPaperScissors.play('scissors', 'rock'))
+  def test_rock_paper_player_1()
+    assert_equal(true, RockPaperScissors.rock_paper_player_1('paper', 'rock'))
   end
 
-  def test_play_paper_scissors()
-    assert_equal("Scissors",RockPaperScissors.play('scissors', 'paper'))
-    assert_equal("Scissors",RockPaperScissors.play('paper', 'scissors'))
+  def test_rock_paper_player_2()
+    assert_equal(true, RockPaperScissors.rock_paper_player_2('rock', 'paper'))
+  end
+
+  def test_rock_scissors_player_1()
+    assert_equal(true, RockPaperScissors.rock_scissors_player_1('rock', 'scissors'))
+  end
+
+  def test_rock_scissors_player_2()
+    assert_equal(true, RockPaperScissors.rock_scissors_player_2('scissors', 'rock'))
+  end
+
+  def test_paper_scissors_player_1()
+    assert_equal(true, RockPaperScissors.paper_scissors_player_1('scissors', 'paper'))
+  end
+
+  def test_paper_scissors_player_2()
+    assert_equal(true, RockPaperScissors.paper_scissors_player_2('paper', 'scissors'))
   end
 end
