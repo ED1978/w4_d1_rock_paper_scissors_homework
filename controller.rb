89c73602bebf43f9df7f9d3079c6e56b892de5dc
EDownie <53910027+ED1978@users.noreply.github.com>
2019-09-16 18:17:01 +0100

    welcome page

--- controller.rb
+++ controller.rb
@@ -5,7 +5,11 @@ require('pry')
 require_relative('./models/rock_paper_scissors.rb')
 also_reload('./models/*')
 
-get '/play/:hand1/:hand2'do
+get '/welcome' do
+  erb (:welcome)
+end
+
+get '/play/:hand1/:hand2' do
   @winner = RockPaperScissors.play(params[:hand1], params[:hand2])
   erb (:result)
 end
